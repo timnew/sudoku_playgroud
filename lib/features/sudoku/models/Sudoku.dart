14dda359cd73c1c6a2030761196c52772a8de560
Tim <timnew.wti@gmail.com>
2020-11-10 21:20:08 +1100

    Remove SudokuCell

--- lib/features/sudoku/models/Sudoku.dart
+++ lib/features/sudoku/models/Sudoku.dart
@@ -1,6 +1,5 @@
 import 'package:built_collection/built_collection.dart';
 import 'package:response_builder/response_builder.dart';
-import 'package:sudoku_playground/features/sudoku/models/SudokuCell.dart';
 import 'package:sudoku_playground/features/sudoku/models/SudokuIndexSet.dart';
 
 import 'SudokuValue.dart';
@@ -8,15 +7,11 @@ import 'SudokuIndex.dart';
 
 typedef SudokuValue ValueBuilder(SudokuIndex index);
 
-class Sudoku extends ResultNotifier<BuiltMap<SudokuIndex, SudokuCell>> {
-  factory Sudoku.fromCells(Iterable<SudokuCell> cells) => Sudoku._(
+class Sudoku extends ResultNotifier<BuiltMap<SudokuIndex, SudokuValue>> {
+  factory Sudoku.fromValues(Iterable<SudokuValue> values) => Sudoku._(
         BuiltMap.of(
-          Map.fromIterables(SudokuIndexSet.allCells(), cells),
+          Map.fromIterables(SudokuIndexSet.allCells(), values),
         ),
-      );
-
-  factory Sudoku.fromValues(Iterable<SudokuValue> values) => Sudoku.fromCells(
-        values.map((e) => SudokuCell(e)),
       );
 
   factory Sudoku.build(ValueBuilder builder) => Sudoku.fromValues(
@@ -27,14 +22,14 @@ class Sudoku extends ResultNotifier<BuiltMap<SudokuIndex, SudokuCell>> {
         (index) => SudokuValue.blank(),
       );
 
-  Sudoku._(BuiltMap<SudokuIndex, SudokuCell> cells) : super(verifyBoard(cells));
+  Sudoku._(BuiltMap<SudokuIndex, SudokuValue> values) : super(verifyBoard(values));
 
-  static BuiltMap<SudokuIndex, SudokuCell> verifyBoard(BuiltMap<SudokuIndex, SudokuCell> cells) {
+  static BuiltMap<SudokuIndex, SudokuValue> verifyBoard(BuiltMap<SudokuIndex, SudokuValue> values) {
     // Check cell count should be sufficient, because:
     // SudokuIndex ensures the index should be valid, the maximum non-duplicated combinations is 81
     // BuiltMap ensures all the index are unique
-    assert(cells.length == 81);
+    assert(values.length == 81);
 
-    return cells;
+    return values;
   }
 }
--- lib/features/sudoku/models/SudokuValue.dart
+++ lib/features/sudoku/models/SudokuValue.dart
@@ -28,44 +28,44 @@ abstract class SudokuValue implements _$SudokuValue {
         guessing: (_) => SudokuValueType.Guessing,
       );
 
-  SudokuValue fullMark() => maybeMap(
-        blank: (_) => SudokuValue.guessing(null, FULL_MARKS),
-        guessing: (v) => v.copyWith(marks: FULL_MARKS),
-        orElse: () => this,
-      );
-
-  SudokuValue addMark(int mark) => maybeMap(
-        blank: (_) => SudokuValue.guessing(null, EMPTY_MARKS.addMark(mark)),
-        guessing: (v) => v.copyWith(marks: v.marks.addMark(mark)),
-        orElse: () => this,
-      );
-
-  SudokuValue removeMark(int mark) => maybeMap(
-        guessing: (v) => v.copyWith(marks: v.marks.removeMark(mark)),
-        orElse: () => this,
-      );
-
-  SudokuValue guess(int number) => maybeMap(
-        blank: (_) => SudokuValue.guessing(number, EMPTY_MARKS),
-        guessing: (v) => v.copyWith(number: number),
-        orElse: () => this,
-      );
-
-  SudokuValue fill(int number) => maybeMap(
-        blank: (_) => SudokuValue.filled(number),
-        guessing: (_) => SudokuValue.filled(number),
-        filled: (_) => SudokuValue.filled(number),
-        orElse: () => this,
-      );
-
-  SudokuValue erase() => maybeMap(
-        guessing: (v) => v.copyWith(number: null),
-        filled: (_) => SudokuValue.blank(),
-        orElse: () => this,
-      );
-
   static BuiltList<bool> EMPTY_MARKS = BuiltList<bool>.of(Iterable.generate(9, (_) => false));
   static BuiltList<bool> FULL_MARKS = BuiltList<bool>.of(Iterable.generate(9, (_) => true));
+
+  SudokuValue fullMark() => maybeMap(
+    blank: (_) => SudokuValue.guessing(null, FULL_MARKS),
+    guessing: (v) => v.copyWith(marks: FULL_MARKS),
+    orElse: () => this,
+  );
+
+  SudokuValue addMark(int mark) => maybeMap(
+    blank: (_) => SudokuValue.guessing(null, EMPTY_MARKS.addMark(mark)),
+    guessing: (v) => v.copyWith(marks: v.marks.addMark(mark)),
+    orElse: () => this,
+  );
+
+  SudokuValue removeMark(int mark) => maybeMap(
+    guessing: (v) => v.copyWith(marks: v.marks.removeMark(mark)),
+    orElse: () => this,
+  );
+
+  SudokuValue guess(int number) => maybeMap(
+    blank: (_) => SudokuValue.guessing(number, EMPTY_MARKS),
+    guessing: (v) => v.copyWith(number: number),
+    orElse: () => this,
+  );
+
+  SudokuValue fill(int number) => maybeMap(
+    blank: (_) => SudokuValue.filled(number),
+    guessing: (_) => SudokuValue.filled(number),
+    filled: (_) => SudokuValue.filled(number),
+    orElse: () => this,
+  );
+
+  SudokuValue erase() => maybeMap(
+    guessing: (v) => v.copyWith(number: null),
+    filled: (_) => SudokuValue.blank(),
+    orElse: () => this,
+  );
 }
 
 extension MarkManipulations on BuiltList<bool> {
